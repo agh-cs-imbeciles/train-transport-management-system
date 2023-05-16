445353033dedbeeb59f51f47916110f369bc0a86
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-16 23:43:13 +0200

    Create `RailRoute` model

--- backend/models/railRoute.js
+++ backend/models/railRoute.js
@@ -1,11 +1,23 @@
 import mongoose from 'mongoose';
+import { PlaceSchema } from './place';
 
 const RailRouteSchema = mongoose.Schema({
     trainId: {
         type: ObjectId,
-        required: [true, 'Train ID is required']
+        required: [true, 'Train ID of the rail route is required']
     },
-    
+    start: {
+        type: PlaceSchema,
+        required: [true, 'Start of the rail route is required']
+    },
+    end: {
+        type: PlaceSchema,
+        required: [true, 'End of the rail route is required']
+    },
+    stops: {
+        type: [PlaceSchema],
+        required: [true, 'Stops of the rail route are required']
+    }
 }, {
     timestamps: true
 });
