d8fc107718c6552d7edcef5ba7aef577bc121e42
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-06-12 00:26:32 +0200

    Get rid of case sensitivity in regular expressions

--- backend/controllers/stops.js
+++ backend/controllers/stops.js
@@ -55,7 +55,7 @@ const getStopByName = async (req, res) => {
     }
 
     const stop = await Stop.findOne({
-        name: { $regex: new RegExp(`.*${stopName}.*`) }
+        name: { $regex: new RegExp(`.*${stopName}.*`, 'i') }
     });
     if (!stop) {
         res.status(404);
@@ -75,10 +75,10 @@ const getStopsByPlace = async (req, res) => {
 
     const matchStage = {};
     if (typeof placeName === 'string') {
-        matchStage.name = { $regex: new RegExp(`.*${placeName}.*`) };
+        matchStage.name = { $regex: new RegExp(`.*${placeName}.*`, 'i') };
     }
     if (typeof provinceName === 'string') {
-        matchStage.province = { $regex: new RegExp(`.*${provinceName}.*`) };
+        matchStage.province = { $regex: new RegExp(`.*${provinceName}.*`, 'i') };
     }
 
     const stops = await Stop.aggregate([
